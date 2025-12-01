615a72c5f842fb6ece14f94cd3977928fbbc1887
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-01 16:37:12 +0200

    added files

--- exe_func.js
+++ exe_func.js
@@ -78,18 +78,103 @@
 // console.log(getProduct())
 
 // ================== 2.1
-function printScore(score) {
-  console.log(`Score is: ${score}`) 
-}
-let score = 80;
-printScore(score)
+// function printScore(score) {
+//   console.log(`Score is: ${score}`)
+// }
+// let score = 80;
+// printScore(score)
 
 // ================== 2.2
+// let grade2 = 50
+// function increaseGrade(grade) {
+//   grade2 += 10
+//   console.log(grade2)
+// }
+// increaseGrade(grade2);
 // ================== 2.3
+// let counter = 0
+// function incrementCounter() {
+//   counter += 1
+//   console.log(counter)
+// }
+// incrementCounter()
+// console.log(counter)
+
 // ================== 2.4
+// let name = "Outer"
+// function printName(name) {
+//   console.log(name)
+// }
+// printName("Inner")
 // ================== 2.5
+// function createSecret(){
+//   let secret = 1234
+//   console.log(secret)
+// }
+// console.log(secret)
 // ================== 2.6
+// function trash(a,b) {
+//   return a + b
+// }
+// let a = 100
+// console.log(trash(5,7))
 // ================== 2.7
+// function changeGrade(student) {
+//   student.grade = 100;
+//   console.log(student);
+// }
+// efi = {
+//   Name : 'efraim',
+//   grade : 70
+// }
+// changeGrade(efi)
 // ================== 2.8
+// function sumOfNumbers(list) {
+//   return  list.reduce((acc, num) => acc + num, 0);
+// }
+// list = [1,2,3,4,5]
+// console.log(sumOfNumbers(list))
 // ================== 2.9
+// function printMassage(massage) {
+//   if (typeof massage == 'string') {
+//     console.log(massage)
+//     return
+//   }
+//   else {
+//     console.log('No massage')
+//   }
+// }
+
+// printMassage('hi')
+
 // ================== 2.10
+// const baseScore = 50;
+// function addBonus(baseScore) {
+//   console.log(baseScore + 20)
+// }
+// addBonus(baseScore)
+// ================== 3.1
+// let sayHi = () => {console.log('Hi from arrow function')}
+// sayHi()
+// ================== 3.2
+// let doubleNumber = (n) => ({n * 2})
+// let result = doubleNumber(5)
+// console.log(result);
+// ================== 3.3
+// let sumThree = (a,b,c) =>  [a,b,c].reduce((acc, num) => acc + num, 0)
+// console.log(sumThree(5,4,7));
+// ================== 3.4
+// let getFirstElement = (arr) => arr[0]
+// console.log(getFirstElement(["a","b","c"]));
+// ================== 3.5
+// let isAdult = (age) => (age>18)
+// console.log(isAdult(16));
+// console.log(isAdult(20));
+// ================== 3.6
+// let square = (number) => number**2
+// console.log(square(5));
+// ================== 3.7
+
+// ================== 3.8
+// ================== 3.9
+// ================== 3.10
