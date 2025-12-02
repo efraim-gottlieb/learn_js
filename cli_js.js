99f301d01be08c17ed2de999e8d788e960818e53
EfraimGottlieb <efgo583208979@gmail.com>
2025-12-02 17:56:00 +0200

    added

---
+++ cli_js.js
@@ -0,0 +1,10 @@
+console.log(prcess.argv)
+// Create a new anchor element
+const newLink = document.createElement('a');
+
+// Set its attributes
+newLink.href = 'https://www.example.com';
+newLink.textContent = 'Visit Example';
+
+// Append it to the body or another element
+document.body.appendChild(newLink);
